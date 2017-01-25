524e274b2151b6037a46f88b7c39fcf8bf27d932
Connor Murphy <connor.murphy6@marist.edu>
2017-01-24 19:07:02 -0500

    Project 0

---
+++ prj/0/driver_proj0.java
@@ -0,0 +1,14 @@
+//Connor Murphy
+//Project 0
+//1.24.17
+//driver_proj0.java
+
+public class driver_proj0
+{
+	public static void main(String[]args)
+	{
+		System.out.println("Hello World!");
+		
+	}
+	
+}
