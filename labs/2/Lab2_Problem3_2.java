a81fdbef86f5134a4583883f121eb501003e73c8
Connor Murphy <connor.murphy6@marist.edu>
2017-02-01 14:13:07 -0500

    Lab 2

---
+++ labs/2/Lab2_Problem3_2.java
@@ -0,0 +1,61 @@
+//Connor Murphy
+//1.30.17
+//Lab2_Problem3_2.java
+
+import java.util.Random;
+import java.util.Scanner;
+public class Lab2_Problem3_2
+{
+	public static void main (String[]args)
+	{
+		Scanner scan = new Scanner(System.in);
+		Random rand = new Random();
+		
+		int numGuess1 = rand.nextInt(10);
+		int numGuess2 = rand.nextInt(10);
+		int numGuess3 = rand.nextInt(10);
+		int numTries = 0;
+		int total = numGuess1+numGuess2+numGuess3;
+		
+		int guess;
+		boolean winner=false;
+		
+		while(winner==false)
+		{
+			System.out.println("Guess the sum of three numbers 0-28");
+			guess = scan.nextInt();
+			
+			if(guess == total)
+			{
+				winner = true;
+			}
+			
+			else if(guess < total)
+			{
+				System.out.println("your guess is lower then the total");
+			}
+			else if(guess > total)
+			{
+				System.out.println("your guess is higher then the total");
+			}
+			numTries++;
+			
+		}
+		if(numTries >= 8)
+		{
+			System.out.println("Finally you got it .... took you long enough");
+		}
+		else
+		{
+			System.out.println("You Got It!!!!");
+		}
+		System.out.println("it took you "+ numTries + " guesses");
+		System.out.println("the sum of the numbers was "+total);
+		
+	}
+	
+}
+		
+			
+	
+		
